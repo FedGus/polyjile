c3b46e02b47234f3f3b9e77c6ec242e2ce946838
Fedor Gusev <gusevfedya@mail.ru>
2020-07-01 17:43:09 +0300

    Создание бокового меню

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,18 +4,29 @@
 <button @click="show = !show"></button>
 <button>Создать</button>
 </header>
+<div class="main">
 
+<div class="sidebar">
+  <ul>
+    <li><ion-icon name="albums-outline"></ion-icon></li>
+    <li><ion-icon name="clipboard-outline"></ion-icon></li>
+    <li><ion-icon name="calendar-outline"></ion-icon></li>
+    <li><ion-icon name="bug-outline"></ion-icon></li>
+    <li><ion-icon name="bar-chart-outline"></ion-icon></li>
+  </ul>
+</div>
 <transition name="slide-fade">
 <aside v-if="show">
-  <ul>
+  <ul class="menu">
     <li>Главная</li>
     <li>Задачи</li>
+    <li>Календарь</li>
     <li>Тестирование</li>
     <li>Отчеты</li>
   </ul>
 </aside>
 </transition>
-
+<div class="board">
 <section>
   <div class="column">
       <draggable group="tasks" :list="backlog" @change="log">
@@ -58,7 +69,8 @@
       </draggable>
   </div>
 </section>
-
+</div>
+</div>
 <form @submit="addLocation(title, text)">
   <input v-model="title" placeholder="Location Name">
   <input v-model="text" placeholder="Location Image URL">
@@ -123,24 +135,58 @@ header {
 height: 50px;
 box-shadow: 0 0 6px grey;
 position: relative;
-z-index: 1;
+z-index: 3;
 }
 section {
    display: grid;
-    grid-column-gap: 10px;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+   grid-column-gap: 10px;
+   grid-template-columns: repeat(4, minmax(200px, 1fr));
 }
 .column {
   background: #f4f5f7;
+}
+
+.main {
+  display: flex;
 }
 aside {
   width: 200px;
   height: 500px;
   background: #f4f5f7;
   position: absolute;
+  z-index: 1;
+}
+
+.sidebar {
+  width: 56px;
+  background: #f4f5f7;
+  position: relative;
   z-index: 2;
 }
 
+.board {
+  display: flex;
+  flex-direction: column;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+}
+li {
+  list-style: none;
+  padding: 10px 0 10px 10px;
+  height: 25px;
+}
+
+.menu li {
+margin-left: 50px;
+}
+ion-icon {
+  font-size: 20px;
+  color: #172b4d;
+}
 .issue {
     color: #172b4d;
     background-color: #fff;
