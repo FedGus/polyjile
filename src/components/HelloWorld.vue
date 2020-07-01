65ebedd0a956fdedae7071472e78e85861997a44
Fedor Gusev <gusevfedya@mail.ru>
2020-07-01 16:12:55 +0300

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,169 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div>
+  <header>
+<button @click="show = !show"></button>
+<button>Создать</button>
+</header>
+
+<transition name="slide-fade">
+<aside v-if="show">
+  <ul>
+    <li>Главная</li>
+    <li>Задачи</li>
+    <li>Тестирование</li>
+    <li>Отчеты</li>
+  </ul>
+</aside>
+</transition>
+
+<section>
+  <div class="column">
+      <draggable group="tasks" :list="backlog" @change="log">
+        <div class="issue" v-for="(task, idx) in backlog" :key="idx">
+          <h1>{{ task.title }}</h1>
+          <p>{{ task.text }}</p>
+          <span>{{ task.timeAdd }}</span>
+          <button @click="deleteLocation(task.id)">Delete</button>
+        </div>
+      </draggable>
   </div>
+  <div class="column">
+      <draggable group="tasks" :list="inProgress" @change="log">
+       <div class="issue" v-for="(task, idx) in inProgress" :key="idx">
+          <h1>{{ task.title }}</h1>
+          <p>{{ task.text }}</p>
+          <span>{{ task.timeAdd }}</span>
+          <button @click="deleteLocation(task.id)">Delete</button>
+        </div>
+      </draggable>
+  </div>
+  <div class="column">
+      <draggable group="tasks" :list="reviews" @change="log">
+       <div class="issue" v-for="(task, idx) in reviews" :key="idx">
+          <h1>{{ task.title }}</h1>
+          <p>{{ task.text }}</p>
+          <span>{{ task.timeAdd }}</span>
+          <button @click="deleteLocation(task.id)">Delete</button>
+        </div>
+      </draggable>
+  </div>
+  <div class="column">
+      <draggable group="tasks" :list="done" @change="log">
+       <div class="issue" v-for="(task, idx) in done" :key="idx">
+          <h1>{{ task.title }}</h1>
+          <p>{{ task.text }}</p>
+          <span>{{ task.timeAdd }}</span>
+          <button @click="deleteLocation(task.id)">Delete</button>
+        </div>
+      </draggable>
+  </div>
+</section>
+
+<form @submit="addLocation(title, text)">
+  <input v-model="title" placeholder="Location Name">
+  <input v-model="text" placeholder="Location Image URL">
+  <button type="submit">Add New Location</button>
+</form>
+
+</div>
 </template>
 
 <script>
+import { db } from '../main'
+import draggable from 'vuedraggable'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components: {
+    draggable
+  },
+   data () {
+     return {
+       show: false,
+       backlog: [],
+       inProgress: [],
+       reviews: [],
+       done: [],
+       title: '',      // <-- новое свойство
+       text: '',    // <-- новое свойство
+     }
+   },
+   firestore () {
+   return {
+     backlog: db.collection('tasks').where('status', '==', 'backlog'),
+     inProgress: db.collection('tasks').where('status', '==', 'progress'),
+     reviews: db.collection('tasks').where('status', '==', 'reviews'),
+     done: db.collection('tasks').where('status', '==', 'done')
+   }
+   
+ },
+   methods: {
+   addLocation (title, text) {      // <-- новый метод
+     const timeAdd = new Date()
+     const status = 'backlog'
+     db.collection('tasks').add({ title, text, timeAdd, status })
+   },
+    deleteLocation (id) {   // <-- новый метод
+     db.collection('tasks').doc(id).delete()
+   },
+   log: function(evt) {
+      window.console.log(evt);
+      if (evt == 'added') {
+        alert('aaa')
+      }
+    }
+   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+header {
+height: 50px;
+box-shadow: 0 0 6px grey;
+position: relative;
+z-index: 1;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+section {
+   display: grid;
+    grid-column-gap: 10px;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.column {
+  background: #f4f5f7;
 }
-a {
-  color: #42b983;
+aside {
+  width: 200px;
+  height: 500px;
+  background: #f4f5f7;
+  position: absolute;
+  z-index: 2;
+}
+
+.issue {
+    color: #172b4d;
+    background-color: #fff;
+    cursor: move;
+    display: block;
+    font-size: 14px;
+    line-height: 1.42857143;
+    position: relative;
+    box-shadow: 0 0 1px 0 rgba(9,30,66,0.31), 0 2px 4px -1px rgba(9,30,66,0.25);
+    border-radius: 2px;
+    margin: 5px;
+    padding: 10px
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(.65, .05, 0.36, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active до версии 2.1.8 */ {
+  transform: translateX(-200px);
+  opacity: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-Vue.config.productionTip = false
+import { firestorePlugin } from 'vuefire'
+Vue.use(firestorePlugin)
+// Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDggupN1_JJmHlSuMUuCOFbPgHYMxxiHjk",
+  authDomain: "polygile.firebaseapp.com",
+  databaseURL: "https://polygile.firebaseio.com",
+  projectId: "polygile",
+  storageBucket: "polygile.appspot.com",
+  messagingSenderId: "895132686671",
+  appId: "1:895132686671:web:3f2791de935d0cc990cd3a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore()
 
 new Vue({
   render: h => h(App),
